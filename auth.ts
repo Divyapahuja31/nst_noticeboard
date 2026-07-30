f4f33cb7d02a6ffff3d488a80951d304c50666d3
Kanishk Ranjan <kanishkranjan17@gmail.com>
2026-07-30 14:00:41 +0530

    feat: adds email client

--- auth.ts
+++ auth.ts
@@ -1,8 +1,15 @@
 import NextAuth from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import client from "./lib/db"
+import Resend from "next-auth/providers/resend"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(client),
-  providers: [],
+  providers: [
+    Resend({
+      apiKey: process.env.AUTH_RESEND_KEY,
+      from: process.env.EMAIL_FROM || "onboarding@resend.dev",
+    }),
+  ],
+  secret: process.env.AUTH_SECRET || process.env.BETTER_AUTH_SECRET,
 })
